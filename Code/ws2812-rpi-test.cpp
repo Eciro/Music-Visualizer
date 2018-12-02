63d514a6404110f0db9517afd44d8396785913e4
jean <38056663+xiaoyunhua@users.noreply.github.com>
2018-12-01 23:34:45 -0500

    Add files via upload

---
+++ Code/ws2812-rpi-test.cpp
@@ -0,0 +1,66 @@
+/*
+###############################################################################
+#                                                                             #
+# WS2812-RPi                                                                  #
+# ==========                                                                  #
+# A C++ library for driving WS2812 RGB LED's (known as 'NeoPixels' by         #
+#     Adafruit) directly from a Raspberry Pi with accompanying Python wrapper #
+# Copyright (C) 2014 Rob Kent                                                 #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                             #
+###############################################################################
+*/
+
+#include "ws2812-rpi.h"
+
+int main(int argc, char **argv){
+    //64 and under
+    NeoPixel bin1 = NeoPixel(9);
+    //(64, 128]
+    NeoPixel bin2 = NeoPixel(9);
+    //(128, 192]
+    NeoPixel bin3 = NeoPixel(9);
+    //(192, 256]
+    NeoPixel bin4 = NeoPixel(9);
+    //(256, 320]
+    NeoPixel bin5 = NeoPixel(9);
+    //(320, 384]
+    NeoPixel bin6 = NeoPixel(9);
+    //(384, 448]
+    NeoPixel bin7 = NeoPixel(9);
+    //(448, 512]
+    NeoPixel bin8 = NeoPixel(9);
+    //(512, 576]
+    NeoPixel bin9 = NeoPixel(9);
+    //(576, 640]
+    NeoPixel bin10 = NeoPixel(9);
+    //(640, 704]
+    NeoPixel bin11 = NeoPixel(9);
+    //[704, 768]
+    NeoPixel bin12 = NeoPixel(9);
+    //(768, 832]
+    NeoPixel bin13 = NeoPixel(9);
+    //(832, 896]
+    NeoPixel bin14 = NeoPixel(9);
+    //(896, 960]
+    NeoPixel bin15 = NeoPixel(9);
+    //(960, 1024]
+    NeoPixel bin16 = NeoPixel(9);
+    float data = isemisdata;
+    while (true) {
+    }
+
+    return 0;
+}
